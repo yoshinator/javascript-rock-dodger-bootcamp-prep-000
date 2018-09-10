5dfc415e3a1aca37cc81b7032531f28510023169
yoan ante <yoanante@gmail.com>
2018-09-10 02:59:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -83,10 +83,9 @@ function createRock(x) {
       rock.remove()
     }
     }
-  }
-  window.requestAnimationFrame(moveRock)
+  
   // We should kick of the animation of the rock around here
-
+  window.requestAnimationFrame(moveRock)
   // Add the rock to ROCKS so that we can remove all rocks
   // when there's a collision
   ROCKS.push(rock)
